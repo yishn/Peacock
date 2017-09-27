b5299cd62c605ec9718a1cf25f227f8c6256dd8e
Yichuan Shen <shen.yichuan@gmail.com>
2017-09-27 13:10:43 +0200

    Add mutate module

---
+++ src/renderer/mutate.js
@@ -0,0 +1,19 @@
+export default function mutate(obj, change) {
+    let newObj = Array.isArray(obj) ? [...obj] : Object.assign({}, obj)
+
+    for (let key in change) {
+        let value = change[key]
+
+        if (Array.isArray(obj) && isNaN(key)) {
+            newObj[key](...value)
+        } else {
+            newObj[key] = isPrimitive(value) || Array.isArray(value) ? value : mutate(newObj[key], value)
+        }
+    }
+
+    return newObj
+}
+
+export function isPrimitive(value) {
+    return value == null || typeof value !== 'object'
+}
