d8a347f30e2d8ebcd2cf39f7970b42c885f9c43f
Yichuan Shen <shen.yichuan@gmail.com>
2017-09-30 17:26:40 +0200

    Add PaletteList shadows

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -4,15 +4,18 @@ import Palette from './Palette'
 
 export default class PaletteList extends Component {
     render() {
-        return h('ul', {class: 'palette-list'},
-            this.props.palettes.map(palette =>
+        return h('section', {class: 'palette-list'},
+            h('ul', {}, this.props.palettes.map(palette =>
                 h('li', {},
                     h(Palette, {
                         colors: palette.colors.map(x => x.hex)
                     }),
                     h('h3', {}, palette.name)
                 )
-            )
+            )),
+
+            h('div', {class: 'shadow top'}),
+            h('div', {class: 'shadow bottom'})
         )
     }
 }
