15a8068829fdd8596fc9a5d986523c99cae25873
Yichuan Shen <shen.yichuan@gmail.com>
2017-09-30 15:41:16 +0200

    Add focus ring on text boxes

--- src/components/App.js
+++ src/components/App.js
@@ -86,6 +86,77 @@ export default class App extends Component {
                             variants: []
                         }
                     ]
+                },
+                {
+                    name: 'Thought Provoking',
+                    colors: [
+                        {
+                            hex: '#ECD078',
+                            variants: []
+                        },
+                        {
+                            hex: '#D95B43',
+                            variants: []
+                        },
+                        {
+                            hex: '#C02942',
+                            variants: []
+                        },
+                        {
+                            hex: '#542437',
+                            variants: []
+                        },
+                        {
+                            hex: '#53777A',
+                            variants: []
+                        }
+                    ]
+                },
+                {
+                    name: 'Odd Flavor Combos',
+                    colors: [
+                        {
+                            hex: '#B85323',
+                            variants: []
+                        },
+                        {
+                            hex: '#ED6E35',
+                            variants: []
+                        },
+                        {
+                            hex: '#FCA75A',
+                            variants: []
+                        },
+                        {
+                            hex: '#FCB778',
+                            variants: []
+                        },
+                        {
+                            hex: '#F1F2D4',
+                            variants: []
+                        }
+                    ]
+                },
+                {
+                    name: 'Red Poppy Fabric',
+                    colors: [
+                        {
+                            hex: '#F3DDC6',
+                            variants: []
+                        },
+                        {
+                            hex: '#FE7356',
+                            variants: []
+                        },
+                        {
+                            hex: '#E13F3C',
+                            variants: []
+                        },
+                        {
+                            hex: '#8A4F41',
+                            variants: []
+                        }
+                    ]
                 }
             ],
             filter: {
