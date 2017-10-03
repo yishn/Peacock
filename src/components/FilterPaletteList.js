36dbf3e36a251feeb3a98ecfef3fb8c088f5ca92
Yichuan Shen <shen.yichuan@gmail.com>
2017-10-03 22:46:11 +0200

    Implement hue filter

--- src/components/FilterPaletteList.js
+++ src/components/FilterPaletteList.js
@@ -1,8 +1,19 @@
+import chroma from 'chroma-js'
 import {h} from 'preact'
 import comprehend, {from, where, range} from '../renderer/comprehension'
 
 import Component from './PureComponent'
 import PaletteList from './PaletteList'
+
+function getHueDistance(color1, color2) {
+    let [hue1, hue2] = [color1, color2].map(c => chroma(c).get('hsl.h'))
+
+    return Math.min(
+        Math.abs(hue1 - hue2),
+        Math.abs(hue1 - hue2 + 360),
+        Math.abs(hue1 - hue2 - 360)
+    )
+}
 
 export default class FilterPaletteList extends Component {
     constructor(props) {
@@ -18,10 +29,25 @@ export default class FilterPaletteList extends Component {
         let {palettes, filter} = this.props
 
         this.filteredPalettes = comprehend(
-            i => Object.assign({index: i}, palettes[i]),
+            i => Object.assign({
+                index: i,
+                distance: filter.hue == null ? 0 : palettes[i].colors
+                    .map(color => chroma.distance(color.hex, filter.hue))
+                    .reduce((min, x) => Math.min(min, x))
+            }, palettes[i]),
+
             from(range(palettes.length)),
-            where(i => palettes[i].name.toLowerCase().includes(filter.text.trim()))
+            where(i =>
+                palettes[i].name.toLowerCase().includes(filter.text.trim())
+                && (filter.hue == null || palettes[i].colors
+                    .map(color => getHueDistance(color.hex, filter.hue))
+                    .reduce((min, x) => Math.min(min, x)) < 60)
+            )
         )
+
+        if (filter.hue != null) {
+            this.filteredPalettes.sort((p1, p2) => p1.distance - p2.distance)
+        }
 
         return h(PaletteList, Object.assign({}, this.props, {
             filter: undefined,
